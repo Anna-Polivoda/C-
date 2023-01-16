6a694d80e93d459ca69ea89ffa25772046399005
Anna-Polivoda <anny.b@mail.ru>
2023-01-16 21:29:04 +0300

    Added сhanges Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -10,11 +10,11 @@ Console.WriteLine("Введите число 1:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2:");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2)
+if (number1 < number2)
 {
-    Console.WriteLine($"{number1} больше чем {number2}");
+    Console.WriteLine($"max = {number2}");
 }
 else
 {
-    Console.WriteLine($"{number2} больше чем {number1}");
+    Console.WriteLine($"max = {number1}");
 }
