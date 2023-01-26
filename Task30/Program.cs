a597d2b7d3da5453d4085b686f71ee56ea46d6b0
Anna-Polivoda <anny.b@mail.ru>
2023-01-26 22:57:06 +0300

    Added Changes Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -9,9 +9,10 @@ PrintArray(arr);
 int[] CreateArray(int size)
 {
     int[] array = new int[size];
+    Random rnd = new Random();
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(0, 2);
+        array[i] = rnd.Next(0, 2);
     }
     return array;
 }
