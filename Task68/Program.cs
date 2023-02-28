eb2c58ad51c5572186ce1e5f68d34d4cc90e842d
Anna-Polivoda <anny.b@mail.ru>
2023-02-28 13:49:46 +0300

    Added Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -7,26 +7,17 @@ Console.Write("Введите число m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-AkkermanFunction(m,n);
+AkkermanFunction(m, n);
 
 int Akkerman(int m, int n)
 {
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (n == 0)
-    {
-        return Akkerman(m - 1, 1);
-    }
-    else
-    {
-        return (Akkerman(m - 1, Akkerman(m, n - 1)));
-    }
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return (Akkerman(m - 1, Akkerman(m, n - 1)));
 }
 
 void AkkermanFunction(int m, int n)
 {
-    Console.Write($"A({m},{n}) = {Akkerman(m, n)}"); 
+    Console.Write($"A({m},{n}) = {Akkerman(m, n)}");
 }
 
